0a9b29fbf160e93e470e159eb296e5fb949157fb
Hichem Abderrahmane Ali Mahdi <hali-mah@2-D-2.42heilbronn.de>
2025-04-05 19:08:21 +0200

    trying to create the inheretence class in ex01

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 19:01:58 by hali-mah          #+#    #+#             */
+/*   Updated: 2025/04/05 19:07:54 by hali-mah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap(std::string name);
+        ~ScavTrap();
+
+        void    attack(const std::string& target);
+};
+
+#endif
