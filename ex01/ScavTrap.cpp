f0421c922027ce325178456f2e97cb7578660ee8
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-3.42heilbronn.de>
2025-06-18 17:29:53 +0200

    Fixed ex01 with canonical form and added some proper tests

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,38 +6,66 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 19:02:33 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/05 19:18:55 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/06/18 17:21:46 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
+// Default constructor
+ScavTrap::ScavTrap() : ClapTrap()
 {
     _Hit_points = 100;
     _Energy_points = 50;
     _Attack_damage = 20;
-    std::cout << "ScavTrap " << _Name << " Ready for action!\n" << std::endl;
+    std::cout << "ScavTrap " << _Name << " default constructed\n";
 }
 
+// Parameterized constructor
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
+{
+    _Hit_points = 100;
+    _Energy_points = 50;
+    _Attack_damage = 20;
+    std::cout << "ScavTrap " << _Name << " Ready for action!\n";
+}
+
+// Copy constructor
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
+{
+    std::cout << "ScavTrap " << _Name << " copied\n";
+}
+
+// Copy assignment operator
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+    if (this != &other)
+    {
+        ClapTrap::operator=(other);
+    }
+    std::cout << "ScavTrap assignment operator called\n";
+    return *this;
+}
+
+// Destructor
 ScavTrap::~ScavTrap()
 {
-    std::cout << "ScavTrap " << _Name << " Work down, GoodBye!\n" << std::endl;
+    std::cout << "ScavTrap " << _Name << " Work done, Goodbye!\n";
 }
 
 void ScavTrap::attack(const std::string& target)
 {
-    if(_Hit_points <= 0 || _Energy_points <= 0)
+    if (_Hit_points <= 0 || _Energy_points <= 0)
     {
-        std::cout << "ScavTrap " << _Name << " has no energy or HP left to attack!" << std::endl;
+        std::cout << "ScavTrap " << _Name << " has no energy or HP left to attack!\n";
         return;
     }
     _Energy_points--;
-    std::cout << "ScavTrap " << _Name << " aggressively attacks " << target 
-              << ", causing " << _Attack_damage << " points of devastating damage!\n" << std::endl;
+    std::cout << "ScavTrap " << _Name << " aggressively attacks " << target
+              << ", causing " << _Attack_damage << " points of devastating damage!\n";
 }
 
 void ScavTrap::guardGate()
 {
-    std::cout << "ScavTrap " << _Name << " is now in Gate keeper mode. No one shall pass!\n" << std::endl;
+    std::cout << "ScavTrap " << _Name << " is now in Gate keeper mode. No one shall pass!\n";
 }
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,23 +6,26 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 19:01:58 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/05 19:13:40 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/06/18 17:21:31 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
-# define SCAVTRAP_HPP
+#define SCAVTRAP_HPP
 
 #include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap
 {
-    public:
-        ScavTrap(std::string name);
-        ~ScavTrap();
+public:
+    ScavTrap();
+    ScavTrap(const std::string& name);
+    ScavTrap(const ScavTrap& other);
+    ScavTrap& operator=(const ScavTrap& other);
+    ~ScavTrap();
 
-        void    attack(const std::string& target);
-        void    guardGate();
+    void attack(const std::string& target);
+    void guardGate();
 };
 
 #endif
